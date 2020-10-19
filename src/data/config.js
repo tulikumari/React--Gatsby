31986cfce8887557beda485941da460a3c5f9579
tulikumari <info.tulikumari@gmail.com>
2020-10-19 12:02:32 +0530

    initial comment

---
+++ src/data/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  defaultTitle: 'Tuli Kumari',
+  logo: 'http://glaxom.com/projects/icon/favicon.png',
+  author: 'Tuli Kumari',
+  url: 'https://glaxom.com',
+  legalName: 'Tuli Kumari',
+  defaultDescription: 'I’m Tuli Kumari and I’m a Full Stack Developer!',
+  socialLinks: {
+    github: 'https://github.com/tulikumari',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: '',
+    twitter: '',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+  recaptcha_key: '6LdG6tgZAAAAADvHX_iF17EuQ_rWXuMRDqxFQ2di',
+};
